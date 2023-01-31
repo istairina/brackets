9c7ded7b6e8a863665f63fdf00401e2148fc53b1
Irina Migunova <ista.irina@gmail.com>
2023-01-31 23:56:49 +0300

    feat: finish task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    let obj = {}; // {) : (}
+    bracketsConfig.forEach((pair) => {
+      obj[pair[1]] = pair[0];
+    })
+    let openArr = Object.values(obj);
+    let closeArr = Object.keys(obj);
+    let glass = [];
+    for (let i = 0; i < str.length; i++) {
+      if (openArr.indexOf(str[i]) >= 0 && str[i] != obj[str[i]]) {
+        glass.push(str[i]);
+      } else if (closeArr.indexOf(str[i]) >= 0 && str[i] != obj[str[i]]) {
+        if (glass[glass.length - 1] == obj[str[i]]) {
+          glass.pop();
+        } else return false;
+      } else if (str[i] == obj[str[i]]) {
+        if (glass.indexOf(str[i]) < 0) {
+          glass.push(str[i]);
+        } else if (glass[glass.length - 1] == str[i]) {
+          glass.pop();
+        } else return false;
+      }
+    }
+    if (glass.length == 0) {
+      return true;
+    } else return false;
+  }
